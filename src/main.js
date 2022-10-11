e26d6a25403b2b1771c5321e6bb6bbb9ce3e1100
Caique Torres <caiquetorres.bastos@hotmail.com>
2022-10-11 00:30:39 -0300

    feat(app.router.js): implemented the "noiseRouter" in the application routes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-import express from 'express'
 import appRouter from './app.router'
 
+import express from 'express'
+
 const app = express()
+
+app.use(express.json())
+
 app.use(appRouter)
 
 app.listen(3000)
