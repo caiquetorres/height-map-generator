c08191dc79efbf6fa1f8446066115c97d44f13ca
Caique Torres <caiquetorres.bastos@hotmail.com>
2022-10-11 16:50:36 -0300

    feat(fft.js): created the methods "swapQuadrants" and "shift"

--- src/fft/fft.js
+++ src/fft/fft.js
@@ -34,4 +34,53 @@ export class Fft {
   inverseTransform(frequencies) {
     return this.strategy.inverseTransform(frequencies)
   }
+
+  /**
+   * Method that swaps the first with the third quadrant and the second with the fourth one.
+   *
+   * @param {unknown[]} signal defines an array.
+   */
+  swapQuadrants(signal) {
+    const size = Math.sqrt(signal.length)
+
+    for (let y = 0; y < size / 2; y++) {
+      for (let x = 0; x < size / 2; x++) {
+        const index1 = y * size + x
+        const index2 = (y + size / 2) * size + (x + size / 2)
+
+        const aux = signal[index1]
+        signal[index1] = signal[index2]
+        signal[index2] = aux
+      }
+    }
+
+    for (let y = 0; y < size / 2; y++) {
+      for (let x = size / 2; x < size; x++) {
+        const index1 = y * size + x
+        const index2 = (y + size / 2) * size + (x - size / 2)
+
+        const aux = signal[index1]
+        signal[index1] = signal[index2]
+        signal[index2] = aux
+      }
+    }
+  }
+
+  /**
+   * Method that shifts the array values to the center using the transforms 3/8 for height and 1/4 for width.
+   *
+   * @param {unknown[]} signal defines an array.
+   */
+  shift(signal) {
+    const size = Math.sqrt(signal.length)
+
+    for (let y = 0; y < size; y++) {
+      for (let x = 0; x < size; x++) {
+        const index1 = y * size + x
+        const index2 = (y + (3 * size) / 8) * size + (x + size / 4)
+
+        signal[index1] = signal[index2]
+      }
+    }
+  }
 }
