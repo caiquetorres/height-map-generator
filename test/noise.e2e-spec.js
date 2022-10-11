ea5e76893bbf43c2de2e80cd33e9a8d198b4b83b
Caique Torres <caiquetorres.bastos@hotmail.com>
2022-10-11 00:57:56 -0300

    test(noise.service.spec.ts & noise.e2e-spec.ts): added some tests to ensure that the white noise generator is working as expected

---
+++ test/noise.e2e-spec.js
@@ -0,0 +1,35 @@
+import appRouter from '../src/app.router'
+
+import { createApp } from '../src/base'
+import Jimp from 'jimp/'
+import path from 'path'
+import supertest from 'supertest'
+
+describe('Noise (e2e)', () => {
+  let app
+
+  beforeEach(() => {
+    app = createApp()
+    app.use(appRouter)
+  })
+
+  it('/POST /white-noise', async () => {
+    const { body } = await supertest(app)
+      .post('/white-noise')
+      .send({
+        size: 128,
+        seed: 'frequency domain',
+      })
+      .expect(200)
+
+    expect(body).toBeInstanceOf(Buffer)
+
+    const expectedImage = await Jimp.read(
+      path.resolve(__dirname, '../assets/white-noise.png'),
+    ).then((res) => res.bitmap.data)
+
+    const receivedImage = await Jimp.read(body).then((res) => res.bitmap.data)
+
+    expect(receivedImage).toEqual(expectedImage)
+  })
+})
