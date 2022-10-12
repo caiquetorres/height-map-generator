608d9e41dcad290c16bac8b1bf9cea6e3f469da3
Caique Torres <caiquetorres.bastos@hotmail.com>
2022-10-11 21:06:23 -0300

    feat: replaced all the "png" images to "jpeg" ones

--- test/fft.e2e-spec.js
+++ test/fft.e2e-spec.js
@@ -17,13 +17,13 @@ describe('Fft (e2e)', () => {
     it('should convert an image to the frequency domain', async () => {
       const { body } = await supertest(app)
         .post('/fft')
-        .attach('file', path.resolve(__dirname, '../assets/white-noise.png'))
+        .attach('file', path.resolve(__dirname, '../assets/white-noise.jpg'))
         .expect(200)
 
       expect(body).toBeInstanceOf(Buffer)
 
       const expectedImage = await Jimp.read(
-        path.resolve(__dirname, '../assets/white-noise-frequency-domain.png'),
+        path.resolve(__dirname, '../assets/white-noise-frequency-domain.jpg'),
       ).then((res) => res.bitmap.data)
 
       const receivedImage = await Jimp.read(body).then((res) => res.bitmap.data)
