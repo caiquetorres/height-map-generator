85fb51b9c8b91bc1f4316aa653d696fe0e49ab91
Caique Torres <caiquetorres.bastos@hotmail.com>
2022-10-11 21:00:06 -0300

    feat(nyquist.js): created this class to handle the logic for generating nyquist sequences

---
+++ src/common/classes/nyquist.js
@@ -0,0 +1,39 @@
+export class Nyquist1D extends Array {
+  constructor(length) {
+    super()
+
+    let start = 0
+    let end = length / 2 - 1
+
+    for (let i = start; i <= end; i++) {
+      this.push(i / length)
+    }
+
+    start = -length / 2
+    end = -1
+
+    for (let i = start; i <= end; i++) {
+      this.push(i / length)
+    }
+  }
+}
+
+export class Nyquist2D extends Array {
+  constructor(length) {
+    super(length)
+
+    for (let i = 0; i < length; i++) {
+      this[i] = new Array(length)
+    }
+
+    const nyquist = new Nyquist1D(length)
+
+    for (let y = 0; y < length; y++) {
+      for (let x = 0; x < length; x++) {
+        this[y][x] = Math.sqrt(
+          nyquist[y] * nyquist[y] + nyquist[x] * nyquist[x],
+        )
+      }
+    }
+  }
+}
