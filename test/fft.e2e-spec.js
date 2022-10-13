9de4b9d6b212f335c941ecce0b97a94da7e8b45d
Caique Torres <caiquetorres.bastos@hotmail.com>
2022-10-13 20:11:34 -0300

    test(fft.e2e-spec.js): validating if the image conversion to the frequency domain is working as expected when validating data

--- test/fft.e2e-spec.js
+++ test/fft.e2e-spec.js
@@ -27,5 +27,22 @@ describe('Fft (e2e)', () => {
 
       expect(receivedImage).toEqual(expectedImage)
     })
+
+    it('should get 400 (Bad Request) when trying to converting some image to the frequency domain without sending the file', async () => {
+      const { body } = await supertest(app).post('/fft').expect(400)
+
+      expect(body).toHaveProperty('statusCode')
+      expect(body).toHaveProperty('message')
+    })
+
+    it('should get 415 (Unsupported Media Type) when trying to converting some image to the frequency domain sending an invalid file', async () => {
+      const { body } = await supertest(app)
+        .post('/fft')
+        .attach('file', path.resolve(__dirname, '../package.json'))
+        .expect(415)
+
+      expect(body).toHaveProperty('statusCode')
+      expect(body).toHaveProperty('message')
+    })
   })
 })
