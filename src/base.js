792f4d18010189a5bd4234deedeb132ee7fc37bf
Caique Torres <caiquetorres.bastos@hotmail.com>
2022-10-13 12:57:59 -0300

    feat(main.js & base.js): implemented the "errorHandler" middleware

--- src/base.js
+++ src/base.js
@@ -1,9 +1,16 @@
+import appRouter from './app.router'
+
+import { errorHandler } from './common/errors/error.handler'
+import cors from 'cors'
 import express from 'express'
 
 export function createApp() {
   const app = express()
 
+  app.use(cors())
   app.use(express.json())
+  app.use(appRouter)
+  app.use(errorHandler)
 
   return app
 }
--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
-import appRouter from './app.router'
-
 import { createApp } from './base'
 
 const app = createApp()
-
-app.use(appRouter)
 
 app.listen(3000)
